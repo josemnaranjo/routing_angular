629a521de8c03c7c389c32ca58cca22e898cf98c
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-01-30 12:59:43 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes , RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { CategoryComponent } from './category/category.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+const appRoutes:Routes=[
+    {path:'',component:HomeComponent},
+    {path:'home',component:HomeComponent},
+    {path:'products',component:ProductsComponent},
+    {path:'users',component:UsersComponent},
+    {path:'**',component:ErrorPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    UsersComponent,
+    CategoryComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
